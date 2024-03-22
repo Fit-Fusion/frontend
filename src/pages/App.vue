54729f39318ba4e7741fe51033420b2284aa4f6e
mayorCoder <adebayomayowa78@gmail.com>
2024-03-22 11:22:57 +0300

    first commit

---
+++ src/pages/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="page-container">
+        <Header />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '../components/Header.vue';
+
+@Component({
+    components: {
+        Header
+    }
+}) 
+export default class App extends Vue {
+    public name = 'App';
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/styles";
+</style>
