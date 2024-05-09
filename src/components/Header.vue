8dfaffbfc0953f8144d1f96a5b570b86d36f5303
mayorCoder <adebayomayowa78@gmail.com>
2024-05-09 21:23:10 +0300

    Made Header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,7 @@
 <template>
     <header class="header">
         <Logo /> 
-        <HeaderNav />
-        <button class="header__btn">
-            <router-link 
-                to="/login" 
-                class="header__link"
-            >
-                Login
-            </router-link>
-        </button>
+        <HeaderNav v-if="showNav"/>
     </header>
 </template>
 
@@ -25,7 +17,12 @@ import HeaderNav from './HeaderNav.vue';
     }
 })
 export default class Header extends Vue {
-    public name = 'Header'; 
+    public name = 'Header';
+    public showNav = true;
+    
+    public toggleNav() {
+        this.showNav = !this.showNav;
+    }
 }
 </script>
 
@@ -37,22 +34,7 @@ export default class Header extends Vue {
     @include size($width: 100%, $height: 6rem);
     background-color: $theme-background;
     padding: 0 1rem 0 1rem;
-
+    height: 15vh;
     box-sizing: border-box;
-
-    &__btn {
-        background-color: $theme-color;
-        font-weight: 600;
-        font-family:'Courier New', Courier, monospace;
-        padding: 0.5rem 1rem;
-        border: none;
-        cursor: pointer;
-        border-radius: 0.3rem; 
-    }
-
-    &__link {
-        color: $black;
-        text-decoration: none;
-    }
 }
 </style>
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -29,22 +29,34 @@
         </li>
         <li class="header-nav__item">
             <router-link 
-                to="/blog" 
+                to="/calorie-calculator" 
                 class="header-nav__link"
-                :class="{ 'active': $route.path === '/blog' }"
+                :class="{ 'active': $route.path === '/calorie-calculator' }"
             >
-                Blog
+                Calorie Calculator
             </router-link>
+        </li>
+        <li class="header-nav__item">
+            <button class="header-nav__btn">
+                <router-link 
+                    to="/login" 
+                    class="header-nav__btn-link"
+                >
+                    Login
+                </router-link>
+            </button>
         </li>
     </ul>
 </template>
 
 <script lang="ts">
-import { Vue, Component} from 'vue-property-decorator';
+import { Vue, Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class HeaderNav extends Vue {
     public name = 'HeaderNav';   
+
+    @Prop({ default: false }) showNav: boolean;
 
 }
 </script>
@@ -55,9 +67,26 @@ export default class HeaderNav extends Vue {
 .header-nav {
     @include flex($direction: row, $justify-content: space-evenly, $align-items: center);
     width: 50%;
+
+    @include screen-for($mobile) {
+        flex-direction: column;
+        align-items:flex-end;
+        margin-top: 10rem;
+        width: 30%;
+        background: $theme-background;
+    }
+
+    @include screen-including($mobile, $tablet) {
+        width: 65%;
+    }
     
     &__item {
         @include text-format($weight: 300, $size: 1rem);
+
+        
+        @include screen-for($mobile) {
+            padding: 0 1rem 1rem 0;
+        }
 
         list-style-type: none;
         cursor: pointer;
@@ -76,6 +105,25 @@ export default class HeaderNav extends Vue {
 
     &__link.active {
         color: $theme-color;
+    }
+
+    &__btn {
+        background-color: $theme-color;
+        font-weight: 600;
+        font-family:'Courier New', Courier, monospace;
+        padding: 0.5rem 1rem;
+        border: none;
+        cursor: pointer;
+        border-radius: 0.3rem; 
+
+        &-link {
+            color: $black;
+            text-decoration: none;
+        }
+    }
+
+    &__btn:hover {
+        background-color: $theme-color-hover;
     }
 }
 </style>
